fe867352cb438d9e837027f83273166e3f3cf031
DESKTOP-DBQBMC5\Admin <vungocduc597@gmail.com>
2022-08-15 13:19:41 +0700

    T001: Login Screens

---
+++ demo/src/main/java/com/example/demo/model/UserService.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import com.example.SpringBootLogin.model.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+   @Autowired
+   UserRepository userRepository;
+    public UserDetails loadUserByUsername(String username)  {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new CustomUserDetails(user);
+    }
+}
