f51ef01fc54557fea285eef438a45a6faa3e11c0
DESKTOP-DBQBMC5\Admin <vungocduc597@gmail.com>
2022-08-15 14:30:10 +0700

    T001:Login Screens

--- demo/src/main/java/com/example/demo/model/UserService.java
+++ demo/src/main/java/com/example/demo/model/UserService.java
@@ -1,6 +1,5 @@
 package com.example.demo.model;
 
-import com.example.SpringBootLogin.model.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
